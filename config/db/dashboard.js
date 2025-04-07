733dc2d2017fc19761fcb774c94df8d4356081e6
ngan23github <ledoankimngan2005@gmail.com>
2025-04-07 18:05:09 +0700

    đi chơi cx làm

--- config/db/dashboard.js
+++ config/db/dashboard.js
@@ -2,7 +2,63 @@
 import pool from '../index.js';
 
 class Dashboard{
-    
+    // Hiển thị thông tin chung
+    async get_provider_recently(){
+        const query = 
+        `SELECT ncc.TenNCC
+        FROM hoadonnhap
+        LEFT JOIN ncc ON hoadonnhap.ID_NCC = ncc.ID_NCC
+        ORDER BY ABS(DATEDIFF(NgayNhap, CURDATE()))
+        LIMIT 8`;
+        const [rows] = await pool.execute(query);
+        return rows;
+    }
+
+    async get_receipt_recently(){
+        const query = 
+        `SELECT IDHoaDonNhap, TongTien, TinhTrangThanhToan
+        FROM hoadonnhap
+        ORDER BY ABS(DATEDIFF(NgayNhap, CURDATE()))
+        LIMIT 9`;
+        const [rows] = await pool.execute(query);
+        return rows;
+    }
+
+    async get_sum_of_product(){
+        const query = 
+        `SELECT COUNT(SanPhamID) as SLsanpham
+        FROM sanpham 
+        WHERE tinhTrang = 1`;
+        const [rows] = await pool.execute(query);
+        return rows;
+    }
+
+    async get_sum_of_provider(){
+        const query = 
+        `SELECT COUNT(ID_NCC) as SLncc
+        FROM ncc 
+        WHERE tinhTrang = 1`;
+        const [rows] = await pool.execute(query);
+        return rows;
+    }
+
+    async get_sum_of_receipt(){
+        const query = 
+        `SELECT COUNT(IDHoaDonNhap) as SLhoadon 
+        FROM hoadonnhap 
+        WHERE tinhTrang = 1`;
+        const [rows] = await pool.execute(query);
+        return rows;
+    }
+
+    async get_sum_of_paid_money(){
+        const query = 
+        `SELECT SUM(TongTien) as TongHoaDon
+        FROM hoadonnhap 
+        WHERE tinhTrang = 1`;
+        const [rows] = await pool.execute(query);
+        return rows;
+    }
 }
 
 // module.exports = new Dashboard();
--- controller/DashboardController.js
+++ controller/DashboardController.js
@@ -5,7 +5,20 @@ class DashboardController{
     // show the dashboard
     async index(req, res){
         try {
-            res.render('dashboard');
+            const sum_product = (await dashboardConfig.get_sum_of_product())[0];
+            const sum_provider = (await dashboardConfig.get_sum_of_provider())[0];
+            const sum_receipt = (await dashboardConfig.get_sum_of_receipt())[0];
+            const sum_paid_money = (await dashboardConfig.get_sum_of_paid_money())[0];
+            const provider_recently = await dashboardConfig.get_provider_recently();
+            const receipt_recently = await dashboardConfig.get_receipt_recently();
+            res.render('dashboard',{
+                sum_product,
+                sum_provider,
+                sum_receipt,
+                sum_paid_money,
+                receipt_recently,
+                provider_recently
+            });
         } catch (error) {
             console.log(error);
         }
