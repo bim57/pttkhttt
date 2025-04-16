32458879e512c8858df0e178334b9506189f1e25
ngan23github <ledoankimngan2005@gmail.com>
2025-04-16 16:59:16 +0700

    new

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,15 @@
 // const mysql = require('mysql2');
+// const dotenv = require('dotenv');
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // cài đặt kết nối mysql 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "mysql1009",
-    database: "cua_hang_sach",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
 // module.exports = pool.promise();
